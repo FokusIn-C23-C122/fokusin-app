6433eb8d055108fde44080e928515ada997e7185
ryujinnnee <beranihjrah1@gmail.com>
2023-06-13 22:52:12 +0700

    fix screen force close

--- app/src/main/java/com/capstone/fokusin/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/capstone/fokusin/ui/fragment/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.capstone.fokusin.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.capstone.fokusin.R
+import com.capstone.fokusin.ui.activity.LoginScreen
 
 
 class HomeFragment : Fragment() {
@@ -16,13 +19,18 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val ibtn: ImageView = view.findViewById(R.id.setting)
-
-        ibtn.setOnClickListener {
+        val iv: ImageView = view.findViewById(R.id.setting)
+        val ibtn: Button = view.findViewById(R.id.startButton)
+        iv.setOnClickListener {
             lodFragment(SettingFragment())
+        }
+        ibtn.setOnClickListener {
+            val intent = Intent(activity, LoginScreen::class.java)
+            startActivity(intent)
         }
         return view
     }
+
     private fun lodFragment(fragment: Fragment) {
         val trans = parentFragmentManager.beginTransaction()
         trans.replace(R.id.konten, fragment)
